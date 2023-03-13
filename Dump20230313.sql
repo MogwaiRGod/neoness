b95be51a06667156a07598920fe461cdf2e14b6d
Diane <122520392+MogwaiRGod@users.noreply.github.com>
2023-03-13 12:01:53 +0100

    update bdd

---
+++ Dump20230313.sql
@@ -0,0 +1,112 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: neoness
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `activite_physique`
+--
+
+DROP TABLE IF EXISTS `activite_physique`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `activite_physique` (
+  `id_activite_physique` int unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(200) NOT NULL,
+  `time` int NOT NULL,
+  `image` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_activite_physique`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `activite_physique`
+--
+
+LOCK TABLES `activite_physique` WRITE;
+/*!40000 ALTER TABLE `activite_physique` DISABLE KEYS */;
+/*!40000 ALTER TABLE `activite_physique` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seance`
+--
+
+DROP TABLE IF EXISTS `seance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `seance` (
+  `id_seance` int unsigned NOT NULL AUTO_INCREMENT,
+  `time` int NOT NULL,
+  `id_activite` int unsigned NOT NULL,
+  PRIMARY KEY (`id_seance`),
+  KEY `id_activite` (`id_activite`),
+  CONSTRAINT `id_activite` FOREIGN KEY (`id_activite`) REFERENCES `activite_physique` (`id_activite_physique`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seance`
+--
+
+LOCK TABLES `seance` WRITE;
+/*!40000 ALTER TABLE `seance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `seance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id_user` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `tel` varchar(20) NOT NULL,
+  `poids` decimal(5,2) NOT NULL,
+  `taille` decimal(3,2) NOT NULL,
+  `objectif` decimal(5,2) NOT NULL,
+  `identifiant` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `id_seance` int unsigned NOT NULL,
+  `user_pseudo` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_user`),
+  KEY `id_seance_idx` (`id_seance`),
+  CONSTRAINT `id_seance` FOREIGN KEY (`id_seance`) REFERENCES `seance` (`id_seance`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-13 12:01:28
