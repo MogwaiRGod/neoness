ff626a404c3fcb77f9e3347d6030afd678b1c6ce
killard95 <118907811+killard95@users.noreply.github.com>
2023-03-15 13:50:58 +0100

    Add files via upload

---
+++ sport(micka)/sport.sql
@@ -0,0 +1,116 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sport
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `activite_physique`
+--
+
+DROP TABLE IF EXISTS `activite_physique`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `activite_physique` (
+  `id_activite_physique` int unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(200) NOT NULL,
+  `image` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_activite_physique`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `activite_physique`
+--
+
+LOCK TABLES `activite_physique` WRITE;
+/*!40000 ALTER TABLE `activite_physique` DISABLE KEYS */;
+INSERT INTO `activite_physique` VALUES (1,'tennis','https://cdn-icons-png.flaticon.com/128/10/10575.png'),(2,'foot','https://cdn-icons-png.flaticon.com/512/27/27127.png'),(3,'course de fond','https://cdn-icons-png.flaticon.com/128/3018/3018580.png'),(4,'natation','https://cdn-icons-png.flaticon.com/512/47/47743.png'),(5,'combat','https://cdn-icons-png.flaticon.com/128/37/37457.png'),(6,'musculation','https://cdn-icons-png.flaticon.com/512/185/185590.png'),(7,'gymnastique','https://cdn-icons-png.flaticon.com/512/94/94171.png'),(8,'badminton','https://cdn-icons-png.flaticon.com/512/10/10566.png'),(9,'aviron','https://cdn-icons-png.flaticon.com/512/38/38607.png');
+/*!40000 ALTER TABLE `activite_physique` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seance`
+--
+
+DROP TABLE IF EXISTS `seance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `seance` (
+  `id_seance` int unsigned NOT NULL AUTO_INCREMENT,
+  `time` int unsigned NOT NULL,
+  `date` date NOT NULL,
+  `id_user` int unsigned NOT NULL,
+  `id_activite` int unsigned NOT NULL,
+  PRIMARY KEY (`id_seance`),
+  KEY `id_user_idx` (`id_user`),
+  KEY `id_activite_idx` (`id_activite`),
+  CONSTRAINT `id_activite` FOREIGN KEY (`id_activite`) REFERENCES `activite_physique` (`id_activite_physique`),
+  CONSTRAINT `id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seance`
+--
+
+LOCK TABLES `seance` WRITE;
+/*!40000 ALTER TABLE `seance` DISABLE KEYS */;
+INSERT INTO `seance` VALUES (1,15,'2023-08-04',1,4),(2,45,'2023-02-24',1,2),(3,5,'2022-01-04',3,1);
+/*!40000 ALTER TABLE `seance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id_user` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `tel` varchar(20) NOT NULL,
+  `poids` decimal(5,2) unsigned NOT NULL,
+  `taille` decimal(3,2) unsigned NOT NULL,
+  `objectif` int unsigned NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `user_pseudo` varchar(45) NOT NULL,
+  `autorisation` set('user','admin') DEFAULT 'user',
+  `avatar` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'ben','micka','55.55.55.55.55',65.00,1.74,60,'azerty','killard','user','https://www.automobile-sportive.com/guide/peugeot/307cc/307cc-ouverture.jpg'),(2,'rob','diane','44.44.44.44.44',55.50,1.70,50,'qwerty','mogwai','admin','https://images.rtl.fr/~c/770v513/rtl/www/1520158-le-personnage-gizmo-image-d-illustration.jpg'),(3,'c','c','122',75.25,1.20,44,'c','c','user','https://cdn-icons-png.flaticon.com/128/1077/1077114.png'),(4,'s','s','2345',55.77,1.34,50,'s','s','user','https://cdn-icons-png.flaticon.com/128/1077/1077114.png'),(5,'x','x','2345',55.87,1.79,46,'x','x','user','https://cdn-icons-png.flaticon.com/128/1077/1077114.png'),(6,'q','q','12345',23.00,4.65,34,'q','q','user','https://cdn-icons-png.flaticon.com/128/1077/1077114.png');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-15 13:45:50
---
+++ sport(micka)/sport_server.js
@@ -0,0 +1,298 @@
+/**
+ * MODULES
+ */
+// pour gérer la BDD mySQL
+const mysql = require('mysql2');
+// on va créer une app express
+const express = require('express');
+// pour lire les requêtes HTTP
+const bodyParser = require('body-parser');
+// moteur de vues
+const ejs = require('ejs');
+
+
+/**
+ * CONSTANTES
+ */
+// nom de la BDD
+const DB = "neoness"; /* A REMPLIR */
+const HOST = '0.0.0.0';
+const PORT = 8080;
+
+
+/**
+ * INSTANCIATIONS
+ */
+const app = express();
+
+/* paramétrage de l'app */
+app.use(bodyParser.urlencoded({ extended: true })); 
+// on demande à ce que les résultats de requêtes soient sous forme d'objets JSON
+app.use(bodyParser.json());
+
+// définition du moteur de vues
+app.set("view engine", "ejs");
+// et du répertoire de vues
+app.set("views", "./views");
+
+// autorisation d'accès aux fichiers statiques du répertoire public
+app.set(express.static("public"));
+
+// instanciation de la connexion à la BDD
+const con = mysql.createConnection({
+    host:'172.17.0.3',
+    user:'Killard',
+    password:'MikaKillard95',
+    database:'sport',
+    multipleStatements: true
+});
+
+// connexion test à la BDD
+con.connect((err) => {
+    if (err) throw err;
+    console.log(`Connecté à la BDD ${DB}`);
+});
+
+
+/**
+ * ROUTES
+ */
+
+// route d'accueil
+app.get('/', (req, res) => {
+    // comment vérifier si un utilisateur est loggé ?
+    // redirige par défaut vers la page de connexion
+    res.redirect('/login');
+});
+
+/* READ */
+// route vers le dashboard admin
+app.get('/admin', (req, res) => {
+    // comment vérifier dans le cache que le client est admin avant d'arriver ici ??
+    // problème : vu que l'admin est directement redirigé ici la connexion, il n'est pas stocké dans le session storage
+
+    // requête pour récupérer les données de tous les utilisateurs
+    let queryAllUsers = "SELECT user_pseudo AS Pseudo, name AS Nom, prenom AS Prénom, tel AS Téléphone, taille AS Taille, poids AS Poids, objectif AS Objectif, pass AS MDP, autorisation AS Droits FROM user;";
+    
+    // connexion à la BDD
+    con.connect((err) => {
+        if (err) throw err;
+
+        // envoi de la requête
+        con.query(queryAllUsers, (err, results) => {
+            if (err) throw err;
+
+            // affichage de la page admin
+            res.render("admin", {
+                'title': "Dashboard admin",
+                'results': results /* envoi des résultats de la requête */
+            });
+        }); // fin con.query
+    }); // fin con.connect
+}); // fin GET /admin
+
+/* CREATE */
+// route recevant le formulaire d'inscription d'utilisateur
+app.post('/signin', (req,res) => {
+    let name = req.body.name;
+    let prenom = req.body.prenom;
+    let tel = req.body.tel;
+    let poids = req.body.poids;
+    let taille = req.body.taille;
+    let objectif = req.body.objectif;
+    let pseudo = req.body.pseudo;
+    let pass = req.body.pass;
+    let avatar = req.body.avatar;
+
+    // on va vérifier que le pseudo demandé n'est pas déjà attribué
+    let checkUser = "SELECT user_pseudo, autorisation, id_user FROM user WHERE user_pseudo = ?" /* ? pour des requêtes sécurisées */;
+    // connexion à la BDD
+    con.connect((err) => {
+        if (err) throw err;
+        // envoi de la requête
+        con.query(checkUser, [pseudo], (err, results) => {
+            if (err) throw err;
+
+            // si la requête n'a pas retourné de résultat <=> si le pseudo est libre
+            if (!results.length){
+                // création d'un objet JSON contenant les informations essentielles de l'utilisateur connecté ;
+                // cet objet va servir de cache dans le session storage client
+                let userStorage = {
+                    'id_user': results[0].id_user,
+                    'username': results[0].pseudo,
+                    'rights': results[0].autorisation
+                }
+                // on peut ++ l'utilisateur à la BDD
+                // let myquery = 'ALTER TABLE user AUTO_INCREMENT = MAX(id_user) ;' 
+                // requête d'ajout utilisateur
+                let myquery = "INSERT INTO user (name, prenom, tel, poids, taille, objectif, pass, user_pseudo, avatar) VALUES (?,?,?,?,?,?,?,?,?) "
+                // connexion BDD
+                con.connect((err)=>{
+                    if (err) throw err;
+                    // envoi requête
+                    con.query(myquery, [name, prenom, tel, poids, taille, objectif, pass, pseudo, avatar], (err,results) => {
+                        // on va immédiatement connecté l'utilisateur
+                        let queryUser = "SELECT * FROM user WHERE user_pseudo = ? AND pass = ?;";
+                        con.query(queryUser, [pseudo, pass], (err, results) => {
+                           // console.log(results[0])
+                            // on envoi l'utilisateur à la page d'accueil
+                            res.render('welcome', { 'title': 'Accueil', 
+                                'message': `Welcome ${prenom}`,
+                                /* et on envoie l'objet JSON du cache */
+                                'storage': userStorage,
+                                'results': results[0]
+                            });
+                        });
+                    });
+                }); 
+            }
+            // sinon, si le pseudo est déjà pris, on envoie un message d'erreur
+            else {
+                res.render('sport_login', {'title': 'Sign In', 'message': 'Inscription', 'erreur': "Pseudo déjà attribué" })
+            }
+        }); // fin con.query
+    }); // fin con.connect
+}); // fin POST /signin
+
+// route affichant la page de log in
+app.get('/login', (req,res) => {
+    res.render("sport_login", {'title': 'Log In', 'message': 'Veuillez entrer vos identifiants afin de vous connecter', 'erreur':""})
+}); // fin GET /login
+
+// route permettant de logger un utilisateur
+app.post('/confirm', (req,res) => {
+    let pseudo = req.body.pseudo;
+    let pass = req.body.pass;
+    // requête qui va sélectionner une rangée dans la BDD selon un pseudo et un mdp (entrés par le client)
+    let myquery = "SELECT * FROM user WHERE user_pseudo = ? AND pass = ?;";
+    // connexion BDD
+    con.connect(function(err){
+        if (err) throw err;
+        // envoi requête
+        con.query(myquery, [pseudo, pass], function(err,results){
+            if (err) throw err;
+            
+            // si la requête a retourné un résultat
+            if(results.length){
+                // on va enregistrer l'utilisateur dans le cache local, donc on crée l'objet JSON à envoyer
+                let userStorage = {
+                    'id_user': results[0].id_user,
+                    'username': results[0].pseudo,
+                    'avatar': results[0].avatar,
+                    'rights': results[0].autorisation
+                }
+                // on évalue le statut de l'utilisateur (autorisation = user ou admin)
+                if (results[0].autorisation == 'admin'){
+                    // si c'est un admin, on le redirige vers le dashboard admin
+                    res.redirect('/admin');
+                }
+                // sinon, si c'est un client normal
+                // on supprime la clef "autorisation" car l'utilisateur n'a pas besoin de connaître ses droits dans la BDD
+                delete results[0].autorisation;
+                // et on le redirigie vers sa page utilisateur
+                res.render('welcome', { 'title': 'Accueil', 
+                    'message': `Welcome ${results[0].prenom}`,
+                    'storage': userStorage, /* envoi du cache */
+                    'results': results[0] /* envoi de toutes ses informations */
+                });
+            } else {
+                // sinon, si les identifiants ne correspondent à rien dans la BDD, on envoie un message d'erreur sur la page de connexion
+                res.render('sport_login' , { 'title' : 'Login', 'message': "Connexion", 'erreur' : 'Identifiant incorrect'})
+            }
+        });
+    });
+}); // fin POST /confirm
+
+/* UPDATE */
+// route recevant les données utilisateur à mettre à jour
+app.post('/update', (req, res) => {
+    let id = req.body.id;
+    let name = req.body.name;
+    let prenom = req.body.prenom;
+    let tel = req.body.tel;
+    let poids = req.body.poids;
+    let taille = req.body.taille;
+    let objectif = req.body.objectif;
+    let pseudo = req.body.pseudo;
+    let pass = req.body.pass;
+    let avatar = req.body.avatar1;
+    let queryUpd = `UPDATE user SET name = '${name}', prenom = '${prenom}', tel = '${tel}', poids = '${poids}', taille = '${taille}', objectif = '${objectif}', pass = '${pass}', user_pseudo = '${pseudo}', avatar = '${avatar}' WHERE id_user = ${id}`;
+    // connexion BDD
+    con.connect((err)=>{
+        if (err) throw err;
+        // on envoie la requête de mise à jour
+        con.query(queryUpd, (err,results)=>{
+            if (err) throw err;
+            // une fois la màj effectuée dans la BDD
+            let queryUser = "SELECT * FROM user WHERE id_user = ?;";
+            
+            con.query(queryUser, [id], (err, results) => {
+                // on réaffiche la page avec les résultats mis à jour
+                res.render('welcome', { 'title': 'Accueil', 
+                    'message': `Welcome ${prenom}`,
+                    /* on n'envoie pas les données du cache car le client est déjà loggé */
+                    'storage': '',
+                    'results': results[0]
+                });
+            })
+        });
+    });
+}); // fin POST /update
+
+/* DELETE */
+// route supprimant un client de la BDD et le redirigeant vers la page de login
+app.post('/deleteUser', (req,res) => {
+    let id = req.body.id;
+    let myquery = `DELETE FROM user WHERE id_user = ${id};`
+    con.connect((err) => {
+        if (err) throw err;
+        con.query(myquery, (err, results) => {
+            if (err) throw err;
+            res.redirect('/login')
+        });
+    });
+}); // fin POST /deleteUser
+
+
+
+// Route pour afficher les séances d'un utilisateur //
+app.post('/seance', (req,res)=>{
+    let id = req.body.id;
+    let myquery = "SELECT user.id_user, user.avatar, user.user_pseudo, type, image, time, DATE_FORMAT(date, '%W %e %M %Y') AS date FROM seance " ;
+    myquery += "INNER JOIN activite_physique ON activite_physique.id_activite_physique = seance.id_activite " ;
+    myquery += "INNER JOIN user ON user.id_user = seance.id_user " ;
+    myquery += `WHERE user.id_user=${id}`
+    console.log(myquery)
+    con.connect((err)=>{
+        if (err) throw err;
+        con.query(myquery, (err,results)=>{
+            if (err) throw err;
+            console.log(results)
+            res.render('sport_seance', {'title': 'liste de vos séances', 'results' : results})
+        });
+    });
+}); //fin POST /seance
+
+app.get('/activity', (req,res)=>{
+    let myquery = "SELECT * FROM activite_physique ";
+    con.connect((err)=>{
+        if (err) throw err;
+        con.query(myquery, (err, results)=>{
+            if (err) throw err;
+            res.send(results)
+        });
+    });
+});
+
+
+
+
+
+
+
+/**
+ * SERVEUR
+ */
+app.listen(PORT, HOST, () => {
+    console.log(`Le serveur tourne sur http://${HOST}:${PORT}`);
+});
